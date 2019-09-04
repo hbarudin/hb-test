37a6a81929443c38c7489480eca14f8620d8a3f3
Hannah Barudin <hbarudin@gmail.com>
2019-09-03 22:29:50 -0400

    Updates

---
+++ precache-manifest.b43762ad4b2bd0b18c487706c147c145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93836e08068b20809396c645bd48f0c",
+    "url": "/hb-test/index.html"
+  },
+  {
+    "revision": "ac191d6122e41283c91c",
+    "url": "/hb-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1874e841e2976b548369",
+    "url": "/hb-test/static/js/2.0501f192.chunk.js"
+  },
+  {
+    "revision": "ac191d6122e41283c91c",
+    "url": "/hb-test/static/js/main.67415236.chunk.js"
+  },
+  {
+    "revision": "60d7ff36c99d8824875c",
+    "url": "/hb-test/static/js/runtime~main.59446eb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hb-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
